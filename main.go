b8eb6bdc1a833c5d9a69efb8b96cfa2e96b776c7
Konstantin <2447785@gmail.com>
2019-03-24 08:49:52 +0300

    Base project

--- main.go
+++ main.go
@@ -1,1 +1,24 @@
-package awesomeProject
+package main
+
+import (
+	"awesomeProject/store"
+	"github.com/gorilla/handlers"
+	"log"
+	"net/http"
+	"os"
+)
+
+func main() {
+	port := os.Getenv("PORT")
+
+	if port == "" {
+		log.Fatal("$PORT must be set")
+	}
+
+	router := store.NewRouter()
+
+	allowedOrigins := handlers.AllowedOrigins([]string{"*"})
+	allowedMethods := handlers.AllowedMethods([]string{"GET", "POST", "DELETE", "PUT"})
+
+	log.Fatal(http.ListenAndServe(":"+port, handlers.CORS(allowedOrigins, allowedMethods)(router)))
+}
